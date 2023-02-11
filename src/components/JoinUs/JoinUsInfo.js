79d0b9318a786e3b13452c19497e04be40d34a92
Chaitu785 <chaitumadane785@gmail.com>
2023-02-11 20:27:41 +0530

    my first commit react website

---
+++ src/components/JoinUs/JoinUsInfo.js
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import Button from "../GlobalComponents/Button";
+
+const JoinUsInfo = ({ title }) => (
+  <div css={styles} className="cardInfo">
+    <h2>{title}</h2>
+    <p>
+    All progress takes place outside the comfort zone.
+    </p>
+    <Button text="JOIN NOW" />
+  </div>
+);
+
+const styles = css`
+  z-index: 3;
+  color: #fff;
+  position: relative;
+  width: 100%;
+  max-width: 380px;
+  text-align: center;
+  margin: 0 auto;
+  h2 {
+    font-size: 36px;
+    font-weight: 800;
+    line-height: 1;
+  }
+  p {
+    font-size: 17px;
+    font-weight: 300;
+    line-height: 1.6;
+    margin: 18px 0 26px 0;
+  }
+  .btn {
+    &:hover {
+      background: transparent;
+      color: #ff1414;
+      border: 1px solid #ff1414;
+    }
+  }
+`;
+
+export default JoinUsInfo;
